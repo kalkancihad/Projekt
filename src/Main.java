a1929668388ac606498cefb34cae5be441329730
cihad.kalkan <cihad.kalkan@accenture.com>
2023-01-13 10:21:12 +0100

    Add method sayHello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        sayHello("Alex");
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello " + name);
     }
 }
